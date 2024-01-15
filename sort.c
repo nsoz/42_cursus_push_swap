cfc61c2745f2a75697711df59c0106b08031306b
Muhammed Enes Oz <muoz@k1m16s04.42istanbul.com.tr>
2024-01-15 18:26:00 +0300

    sortıng has been designed

--- sort.c
+++ sort.c
@@ -2,9 +2,9 @@
 
 int ft_find_closest(t_lst *list, int aforementioned) ///kendinden küçük en yakın datayı gösterir ama kendinden küçük yoksa kendinden büyük en küçük datayı vermeli
 {
-    t_lst *iter;
-	int	distance;
-// 1 2 3 4 5 9 6 8 7 
+    t_lst	*iter;
+	int		distance;
+
     iter = list;
 	distance = 0;
     while (iter != NULL)
@@ -17,21 +17,21 @@ int ft_find_closest(t_lst *list, int aforementioned) ///kendinden küçük en ya
 	if ((iter == NULL) && (distance == 0)) //kendinden büyük en küçük indexi bul
 	{
 		iter = list;
+		distance = 2147483647; 
 		while (iter != NULL)
-		{
-			distance = 2147483647;
+		{												//norm için distance = ft_high_closest() fonksiyonu yap
 			if (aforementioned < iter->index)
 				if (iter->index < distance)
 					distance = iter->index;
 			iter = iter->next;
 		}
 	}
-	// printf("aff: %d, dist: %d\n", aforementioned, distance);
+	printf("aff: %d, dist: %d\n", aforementioned, distance);
 	return (distance);
 }
 
 
-int	ft_find_index(t_lst *list, int aforementioned) // blist 2
+int	ft_find_index(t_lst *list, int aforementioned) // b_list 4
 {
 	int i;
 	t_lst *iter;
@@ -44,44 +44,48 @@ int	ft_find_index(t_lst *list, int aforementioned) // blist 2
 		i++;
 	}
 	if (iter == NULL)
-		return(ft_find_index(list, ft_find_closest(list, (aforementioned))));
+		return(ft_find_index(list, ft_find_closest(list, (aforementioned)))); // findclosest = b_list 0 //ft_find (b_list 2)
 	return (i);
 }
 
 
-int	ft_calculate_in_list(int index, t_lst *list, int len)// 2 blist 3
+int	ft_calculate_in_list(int index, t_lst *list, int len) //4 b_list 3 
 {
 	int i;
 	int distance;
 
-	i = ft_find_index(list, index);// blsit 2 i = 2
-	distance = (len - i);//dis = 1
+	i = ft_find_index(list, index); //a_list 5 
+	distance = (len - i); // 0
 	if (distance < i)
-		return (distance);// 1döndü
+	{
+		if (distance == 0)
+			return (distance + 1); //0                   //listenin son elemanını düzgün hesaplayamıyorduk if else bloğu ile düzelttim ama emin değilim kontrollerini yap
+		else	
+			return (distance); //0
+	}
 	// else if(distance == 0)
 	// 	return (1);
 	else
-		return (i - 1);
+		return (i - 1); //
 }
 
-int	ft_cost_filler(int index, t_lst *b_list, t_lst *a_list, int len)//3 blist alist 3
+int	ft_cost_filler(int index, t_lst *b_list, t_lst *a_list, int len) // 5 b_list a_list 3
 {
-	                        //      3     alist   3                                   2         b       3
-	return ((ft_calculate_in_list(index, a_list, len) + 1) + ft_calculate_in_list((index - 1), b_list, len));
-}
+	return ((ft_calculate_in_list(index, a_list, len) + 1) + ft_calculate_in_list((index - 1), b_list, len)); // 1 a_list 3 && 4 b_lsit 3
+}							//1							+					1          = 2 döndü
 
-int	*ft_cost_calculate(t_lst *a_list, t_lst *b_list, int len)
+int	*ft_cost_calculate(t_lst *a_list, t_lst *b_list, int len) // a_list b_list 3
 {
-	int *cost_of_ret;
+	int 	*cost_of_ret;
 	t_lst	*iter;
-	int i;
+	int 	i;
 
 	i = 0;
 	iter = a_list;
-	cost_of_ret = (int *)malloc(len * sizeof(int));
+	cost_of_ret = (int *)malloc(len * sizeof(int)); //3 lük yer açıldı
 	while (len > i)
 	{
-		cost_of_ret[i] = ft_cost_filler(iter->index, b_list, a_list, len);//3 ,b, a, 3  
+		cost_of_ret[i] = ft_cost_filler(iter->index, b_list, a_list, len); // 5 b_list a_list 3
 		iter = iter->next;
 		i++;
 	}
@@ -102,15 +106,15 @@ t_lst	*ft_sort(t_lst *a_list)
 	int		*cost_of_index;
 	int		len;
 
-	liste_yazdir(a_list, "a LİSTESİ");
+	// liste_yazdir(a_list, "a LİSTESİ");
 	b_list = NULL;
 	do_pb(&a_list, &b_list);
 	do_pb(&a_list, &b_list);
 	len = ft_lst_size(a_list);
-	liste_yazdir(b_list, "B LİSTESİNE İLK İKİ DÜĞÜM EKLENDİ"); // kontrol edildi 
+	// liste_yazdir(b_list, "B LİSTESİNE İLK İKİ DÜĞÜM EKLENDİ"); // kontrol edildi 
 	while(len)
 	{
-		cost_of_index = ft_cost_calculate(a_list, b_list, len);
+		cost_of_index = ft_cost_calculate(a_list, b_list, len);//a_list b_list 3
 		int_arr_yazdır(cost_of_index, len);
 		// ft_processes(a_list, b_list, cost_of_index, len);
 		free(cost_of_index);
